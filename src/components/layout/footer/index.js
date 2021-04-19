3c0359a3b5269d98e8c96d961a5ce62f87878c5f
TRahulSam1997 <rahulsamaranayake@yahoo.com>
2021-04-19 19:48:34 +1000

    footer added

--- src/components/layout/footer/index.js
+++ src/components/layout/footer/index.js
@@ -118,7 +118,7 @@ const Footer = () => {
             <div className="flex flex-wrap items-center md:justify-between justify-center">
             <div className="w-full md:w-4/12 px-4 mx-auto text-center">
                 <div className="text-sm text-gray-600 font-semibold py-1">
-                Copyright © {new Date().getFullYear()}{" "}realmprojectau.com{" "}
+                Copyright © {new Date().getFullYear()}{" "}realmprojectau.com by{" "}
                 <a
                     href="https://www.creative-tim.com"
                     className="text-gray-600 hover:text-gray-900"
