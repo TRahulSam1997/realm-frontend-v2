0cedeeb67ff6af8a001a7d68d02bf29410673900
TRahulSam1997 <rahulsamaranayake@yahoo.com>
2021-04-22 21:13:38 +1000

    footer updates

--- src/components/layout/footer/social/index.js
+++ src/components/layout/footer/social/index.js
@@ -6,34 +6,26 @@ const Styles = styled.div`
 
 `
 
-const SocialIcons = () => {
+export default function SocialIcons() {
     return (
         <Styles>
-            {/* <h1>Test</h1>
             <div className="row">
-            <div className="pr-3">
-                <a href="mailto:ravitejakolla29@gmail.com">
-                    <FontAwesomeIcon icon="envelope" size="2x" />
-                </a>
+                <div className="pr-3">
+                    <a href="https://twitter.com/trsam97">
+                        <FontAwesomeIcon icon={faFacebook} />
+                    </a>
+                </div>
+                <div className="pr-3">
+                    <a href="https://twitter.com/trsam97">
+                        <FontAwesomeIcon icon={faInstagram} />
+                    </a>
+                </div>
+                <div className="pr-3">
+                    <a href="https://twitter.com/trsam97">
+                        <FontAwesomeIcon icon={faTwitter} />
+                    </a>
+                </div>
             </div>
-            <div className="pr-3">
-                <a href="https://www.linkedin.com/in/ravitejakolla">
-                    <FontAwesomeIcon icon={["fab", "linkedin"]} size="2x" />
-                </a>
-            </div>
-            <div className="pr-3">
-                <a href="https://github.com/ravi-kolla">
-                    <FontAwesomeIcon icon={["fab", "github"]} size="2x" />
-                </a>
-            </div>
-            <div className="pr-3">
-                <a href="https://twitter.com/ravitejakolla">
-                    <FontAwesomeIcon icon={["fab", "twitter"]} size="2x" />
-                </a>
-            </div>
-            </div> */}
         </Styles>
     )
 }
-
-export default SocialIcons;
