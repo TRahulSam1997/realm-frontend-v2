6201dd67455f0fa71a57120812a453d5be016540
TRahulSam1997 <rahulsamaranayake@yahoo.com>
2021-04-26 21:47:18 +1000

    footer social update

--- src/components/layout/footer/social/index.js
+++ src/components/layout/footer/social/index.js
@@ -33,8 +33,8 @@ const Styles = styled.div`
         margin-bottom: 20px;
         transition: ease .3s;
         &:hover {
-        color: rgba(167, 146, 129, 1);
-        border: 1px solid rgba(167, 146, 129, 1);
+            color: rgba(167, 146, 129, 1);
+            border: 1px solid rgba(167, 146, 129, 1);
         }
     }
     }
@@ -47,6 +47,26 @@ const Styles = styled.div`
     opacity: 1;
     }
 
+    #instagram {
+        position:relative;
+        top: calc(18% - 10px);
+    }
+
+    #linkedin {
+        position:relative;
+        top: calc(18% - 10px);
+    }
+
+    #youtube {
+        position:relative;
+        top: calc(30% - 10px);
+    }
+
+    #medium {
+        position:relative;
+        top: calc(18% - 10px);
+    }
+
 `
 
 export default function SocialIcons() {
@@ -57,32 +77,32 @@ export default function SocialIcons() {
                     <ul className="social">
                         <li>
                             <a href="https://www.facebook.com/">
-                                <FontAwesomeIcon icon={faFacebook} />
+                                <FontAwesomeIcon icon={faFacebook} id="facebook"/>
                             </a>
                         </li>
                         <li>
                             <a href="https://www.instagram.com/realm.project/">
-                                <FontAwesomeIcon icon={faInstagram} />
+                                <FontAwesomeIcon icon={faInstagram} id="instagram"/>
                             </a>
                         </li>
                         <li>
                             <a href="https://twitter.com/trsam97">
-                                <FontAwesomeIcon icon={faTwitter} />
+                                <FontAwesomeIcon icon={faTwitter} id="twitter"/>
                             </a>
                         </li>
                         <li>
                             <a href="https://www.linkedin.com/company/18713097/">
-                                <FontAwesomeIcon icon={faLinkedin} />
+                                <FontAwesomeIcon icon={faLinkedin} id="linkedin"/>
                             </a>
                         </li>
                         <li>
                             <a href="https://www.youtube.com/rahulsam/">
-                                <FontAwesomeIcon icon={faYoutube} />
+                                <FontAwesomeIcon icon={faYoutube} id="youtube"/>
                             </a>
                         </li>
                         <li>
                             <a href="https://medium.com/@trahulsam/">
-                                <FontAwesomeIcon icon={faMedium} />
+                                <FontAwesomeIcon icon={faMedium} id="medium"/>
                             </a>
                         </li>
                     </ul>
