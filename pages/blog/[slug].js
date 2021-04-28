c8558f8f1e01860a4cc5c8705b7e53f68c51515a
TRahulSam1997 <rahulsamaranayake@yahoo.com>
2021-04-28 21:10:20 +1000

    Added Tooltip to blogpost page

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -6,6 +6,48 @@ import POSTS_WITH_SLUGS from '../../lib/queries/allposts'
 import POST from '../../lib/queries/post'
 import styles from '../../styles/Home.module.css';
 import blogStyles from '../../styles/Blog.module.css';
+import styled from 'styled-components';
+
+const Styles = styled.div`
+    .tooltip {
+        position: relative;
+        display: inline-block;
+        border-bottom: 1px dotted black;
+    }
+
+    .tooltip .tooltiptext {
+        visibility: hidden;
+        width: 120px;
+        background-color: #555;
+        color: #fff;
+        text-align: center;
+        border-radius: 6px;
+        padding: 5px 0;
+        position: absolute;
+        z-index: 1;
+        bottom: 125%;
+        left: 50%;
+        margin-left: -60px;
+        opacity: 0;
+        transition: opacity 0.3s;
+    }
+
+    .tooltip .tooltiptext::after {
+        content: "";
+        position: absolute;
+        top: 100%;
+        left: 50%;
+        margin-left: -5px;
+        border-width: 5px;
+        border-style: solid;
+        border-color: #555 transparent transparent transparent;
+    }
+
+    .tooltip:hover .tooltiptext {
+        visibility: visible;
+        opacity: 1;
+    }
+`;
 
 export default function Post({ postData }) {
     const router = useRouter();
@@ -23,35 +65,38 @@ export default function Post({ postData }) {
     };
 
     return (
-        <div className={styles.container}>
-            <Head>
-                <title>{postData.title}</title>
-                <link rel='icon' href='/favicon.ico' />
-            </Head>
+        <Styles>
+            <div className={styles.container}>
+                <Head>
+                    <title>{postData.title}</title>
+                    <link rel='icon' href='/favicon.ico' />
+                </Head>
 
-            <main className={styles.main}>
-                {router.isFallback ? (
-                    <h2>Loading...</h2>
-                ) : (
-                    <article className={blogStyles.article}>
-                        <div className={blogStyles.postmeta}>
-                            <h1 className={styles.title}>{postData.title}</h1>
-                            <p>{formatDate(postData.date)}</p>
-                            <img src={postData.featuredImage.node.sourceUrl} />
-                        </div>
-                        <div
-                            className='post-content content'
-                            dangerouslySetInnerHTML={{ __html: postData.content }}
-                        />
-                    </article>
-                )}
-                <p>
-                    <Link href={`/blog`}>
-                        <a>Back</a>
-                    </Link>
-                </p>
-            </main>
-        </div>
+                <main className={styles.main}>
+                    {router.isFallback ? (
+                        <h2>Loading...</h2>
+                    ) : (
+                        <article className={blogStyles.article}>
+                            <div className={blogStyles.postmeta}>
+                                <h1 className={styles.title}>{postData.title}</h1>
+                                <p>{formatDate(postData.date)}</p>
+                                <img src={postData.featuredImage.node.sourceUrl} />
+                            </div>
+                            <div
+                                className='post-content content'
+                                dangerouslySetInnerHTML={{ __html: postData.content }}
+                            />
+                        </article>
+                    )}
+                    <p>
+                        <Link href={`/blog`}>
+                            <a>Back</a>
+                        </Link>
+                    </p>
+                </main>
+            </div>
+
+        </Styles>
     )
 }
 
