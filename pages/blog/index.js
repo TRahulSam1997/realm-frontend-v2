9432b4986b6afdce355769c608cb199f3ff1988d
TRahulSam1997 <rahulsamaranayake@yahoo.com>
2021-05-05 22:27:51 +1000

    getting a specific amount of posts

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -10,45 +10,11 @@ const Blog = ({ allPosts: { edges } }) => (
 
         </LoadMorePosts>
     </div>
-
-    // <div className={styles.container}>
-    //     <Head>
-    //         <title>Blog articles page</title>
-    //         <link rel='icon' href='/favicon.ico' />
-    //     </Head>
-
-    //     <main className={styles.main}>
-    //         <h1 className={styles.title}>Latest blog articles</h1>
-    //         <hr />
-    //         <section>
-    //             {edges.map(({ node }) => (
-    //                 <div className={blogStyles.listitem} key={node.id}>
-    //                     <div className={blogStyles.listitem__thumbnail}>
-    //                         <figure>
-    //                             <img
-    //                             src={node.extraPostInfo.thumbImage}
-    //                             alt={node.title}
-    //                             />
-    //                         </figure>
-    //                     </div>
-    //                     <div className={blogStyles.listitem__content}>
-    //                         <h2>{node.title}</h2>
-    //                         <p>{node.extraPostInfo.authorExcerpt}</p>
-    //                         <Link href={`/blog/${node.slug}`}>
-    //                             <a>Read more</a>
-    //                         </Link>
-    //                     </div>
-    //                 </div>
-    //             ))}
-    //         </section>
-    //     </main>
-    // </div>
 );
 
 export default Blog;
 
 export async function getStaticProps() {
-    // const allPosts = await getAllPosts();
 
     const { data } = await client.query({
         query: ALL_POSTS,
