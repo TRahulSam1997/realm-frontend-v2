922a3753c989dc8fd6780bb5cd0ae35f4b61c367
TRahulSam1997 <rahulsamaranayake@yahoo.com>
2021-05-20 20:57:47 +1000

    email collction update

--- src/components/email/emailCollection.js
+++ src/components/email/emailCollection.js
@@ -1,11 +1,12 @@
 import React, { useRef, useState } from 'react';
 import LoadingSpinner from './LoadingSpinner';
+import SuccessMessage from './SuccessMessage';
+import ErrorMessage from './ErrorMessage';
 
 const EmailCollection = () => {
 
     const inputEl = useRef(null);
     const [form, setForm] = useState(false);
-    const [message, setMessage] = useState('');
 
     const subscribe = async (e) => {
         e.preventDefault();
@@ -36,10 +37,9 @@ const EmailCollection = () => {
 
         // 5. Clear the input value and show a success message.
         inputEl.current.value = '';
-        setMessage('Success! You are now subscribed to our Newsletter.');
           setForm({
-          state: 'success'
-          // message: `Hooray! You're now on the list.`
+          state: 'success',
+          message: `Success! You are now subscribed to our Newsletter.`
         });
       };
 
@@ -51,7 +51,7 @@ const EmailCollection = () => {
           </p>
           <form className="relative my-4" onSubmit={subscribe}>
           <input
-              placeholder="tim@apple.com"
+              placeholder="Your email"
               ref={inputEl}
               aria-label="Email for newsletter"
               required
@@ -59,20 +59,19 @@ const EmailCollection = () => {
               autoComplete="email"
               className="px-4 py-2 mt-1 focus:ring-blue-500 focus:border-blue-500 block w-full border-gray-300 rounded-md bg-white dark:bg-gray-800 text-gray-900 dark:text-gray-100"
           />
-          <div>
-              {message
-              ? message
-              : ``}
-          </div>
           <button
             className="flex items-center justify-center absolute right-1 top-1 px-4 font-bold h-8 bg-gray-100 dark:bg-gray-700 text-gray-900 dark:text-gray-100 rounded w-28"
             type="submit"
           >
             {form.state === 'loading' ? <LoadingSpinner /> : 'Subscribe'}
-
           </button>
 
           </form>
+          {form.state === 'error' ? (
+        <ErrorMessage>{form.message}</ErrorMessage>
+      ) : form.state === 'success' ? (
+        <SuccessMessage>{form.message}</SuccessMessage>
+      ) : (null)}
     </div>
     )
 }
