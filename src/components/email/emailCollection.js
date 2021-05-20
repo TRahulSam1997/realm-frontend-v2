9f50dde4907ea387a2b40e1f7e5494037ba486ad
TRahulSam1997 <rahulsamaranayake@yahoo.com>
2021-05-20 20:43:04 +1000

    email collction added for footer

--- src/components/email/emailCollection.js
+++ src/components/email/emailCollection.js
@@ -1,13 +1,15 @@
 import React, { useRef, useState } from 'react';
+import LoadingSpinner from './LoadingSpinner';
 
 const EmailCollection = () => {
 
     const inputEl = useRef(null);
-
+    const [form, setForm] = useState(false);
     const [message, setMessage] = useState('');
 
     const subscribe = async (e) => {
         e.preventDefault();
+        setForm({ state: 'loading' });
 
         // 3. Send a request to our API with the user's email address.
         const res = await fetch('/api/subscribe', {
@@ -24,7 +26,10 @@ const EmailCollection = () => {
 
         if (error) {
           // 4. If there was an error, update the message in state.
-          setMessage(error);
+          setForm({
+            state: 'error',
+            message: error
+          });
 
           return;
         }
@@ -32,29 +37,43 @@ const EmailCollection = () => {
         // 5. Clear the input value and show a success message.
         inputEl.current.value = '';
         setMessage('Success! You are now subscribed to our Newsletter.');
+          setForm({
+          state: 'success'
+          // message: `Hooray! You're now on the list.`
+        });
       };
 
     return (
-        <>
-            <p>Email Collection</p>
-            <form onSubmit={subscribe}>
-        <label htmlFor="email-input">{'Email Address'}</label>
-        <input
-            id="email-input"
-            name="email"
-            placeholder="you@awesome.com"
-            ref={inputEl}
-            required
-            type="email"
-        />
-        <div>
-            {message
-            ? message
-            : `New content, snippets, findings & more.`}
-        </div>
-        <button type="submit">{'Subscribe'}</button>
-        </form>
-    </>
+        <div className="border border-black rounded p-6 my-4 w-full dark:border-gray-800 bg-gray-900 dark:bg-blue-opaque">
+          <p className="text-lg md:text-xl font-bold text-gray-500 dark:text-gray-100">Subscribe to our Newsletter</p>
+          <p className="my-1 text-gray-500 dark:text-gray-200">
+            Get emails on psychology, literature, religion & living truthfully.
+          </p>
+          <form className="relative my-4" onSubmit={subscribe}>
+          <input
+              placeholder="tim@apple.com"
+              ref={inputEl}
+              aria-label="Email for newsletter"
+              required
+              type="email"
+              autoComplete="email"
+              className="px-4 py-2 mt-1 focus:ring-blue-500 focus:border-blue-500 block w-full border-gray-300 rounded-md bg-white dark:bg-gray-800 text-gray-900 dark:text-gray-100"
+          />
+          <div>
+              {message
+              ? message
+              : ``}
+          </div>
+          <button
+            className="flex items-center justify-center absolute right-1 top-1 px-4 font-bold h-8 bg-gray-100 dark:bg-gray-700 text-gray-900 dark:text-gray-100 rounded w-28"
+            type="submit"
+          >
+            {form.state === 'loading' ? <LoadingSpinner /> : 'Subscribe'}
+
+          </button>
+
+          </form>
+    </div>
     )
 }
 
