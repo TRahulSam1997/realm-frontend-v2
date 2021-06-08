64d72dc9266e5eede170ca70b0ec5d5560b1ca9b
TRahulSam1997 <rahulsamaranayake@yahoo.com>
2021-06-08 22:10:12 +1000

    about pages added

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,13 +1,33 @@
 import React from 'react';
 import Layout from '../../src/components/layout/index'
+import ABOUT from '../../lib/queries/pages/about'
+import client from '../../lib/apollo/client'
 
-const About = () => {
+export default function About ({ pageData }) {
   return (
     <Layout>
-        <h1>About</h1>
+        <h1
+          className="font-bold text-3xl md:text-5xl tracking-tight mt-8 mb-4 text-black dark:text-white"
+        >
+          About
+        </h1>
+        <div
+          dangerouslySetInnerHTML={{ __html: pageData.content }}
+        />
     </Layout>
 
   )
 };
 
-export default About;
+export async function getStaticProps() {
+  const { data } = await client.query({
+      query: ABOUT
+    });
+
+  return {
+    props: {
+      pageData: data.page
+    },
+    revalidate: 1
+  };
+}
