ad6fcd570401d201e34af8681bc3fb44ae1c02c9
TRahulSam1997 <rahulsamaranayake@yahoo.com>
2021-06-08 21:58:04 +1000

    pages added

--- pages/truthfully/index.js
+++ pages/truthfully/index.js
@@ -1,14 +1,33 @@
 import React from 'react';
 import Layout from '../../src/components/layout/index'
+import LIVING_TRUTHFULLY from '../../lib/queries/pages/livingTruthfully'
+import client from '../../lib/apollo/client'
 
-const Truthfully = () => {
+export default function Truthfully ({ pageData }) {
   return (
     <Layout>
-        <h1>What is Living Truthfully?</h1>
-        <div><p>Test Test</p></div>
+        <h1
+          className="font-bold text-3xl md:text-5xl tracking-tight mt-8 mb-4 text-black dark:text-white"
+        >
+          What is Living Truthfully?
+        </h1>
+        <div
+          dangerouslySetInnerHTML={{ __html: pageData.content }}
+        />
     </Layout>
 
   )
 };
 
-export default Truthfully;
+export async function getStaticProps() {
+  const { data } = await client.query({
+      query: LIVING_TRUTHFULLY
+    });
+
+  return {
+    props: {
+      pageData: data.page
+    },
+    revalidate: 1
+  };
+}
