fe679b199d9b62ed076c33337d193b2952424975
TRahulSam1997 <rahulsamaranayake@yahoo.com>
2021-06-11 21:56:21 +1000

    living truthfully update

--- pages/truthfully/index.js
+++ pages/truthfully/index.js
@@ -2,19 +2,53 @@ import React from 'react';
 import Layout from '../../src/components/layout/index'
 import LIVING_TRUTHFULLY from '../../lib/queries/pages/livingTruthfully'
 import client from '../../lib/apollo/client'
+import styles from '../../styles/Home.module.css'
+import styled from 'styled-components';
 
+const Styles = styled.div`
+
+    .blogPostBodyText {
+        width: 100%;
+        display: block;
+        font-size: 19px;
+        text-align: left;
+        text-justify: inter-word;
+        padding-top: 0.6em;
+        padding-bottom: 0.2em;
+        /* font-family: libre baskerville, serif; */
+        color: black;
+
+        p {
+            margin-top: 1em;
+            margin-bottom: 1em;
+        }
+    }
+`
 export default function Truthfully ({ pageData }) {
   return (
-    <Layout>
-        <h1
-          className="font-bold text-3xl md:text-5xl tracking-tight mt-8 mb-4 text-black dark:text-white"
-        >
-          What is Living Truthfully?
-        </h1>
-        <div
-          dangerouslySetInnerHTML={{ __html: pageData.content }}
-        />
-    </Layout>
+    <Styles>
+      <Layout>
+        <main className={styles.main}>
+          <h1
+            className="font-bold text-3xl md:text-5xl tracking-tight mt-8 mb-8 text-black dark:text-white"
+          >
+            What is Living Truthfully?
+          </h1>
+          <img
+            src="https://res.cloudinary.com/dkd4xa60a/image/upload/v1623412100/REALM/David_-_The_Death_of_Socrates_iqzfie.jpg"
+            className="w-8/12"
+          />
+          <p>The Death of Socrates | Jacques-Louis David 1787.</p>
+          <div className="w-8/12 mb-32">
+            <div
+              dangerouslySetInnerHTML={{ __html: pageData.content }}
+              className="blogPostBodyText"
+            />
+          <p>~ Rahul</p>
+          </div>
+        </main>
+      </Layout>
+    </Styles>
 
   )
 };
