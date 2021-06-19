8983807c46d828359f39fd364f5f3657a4844b0c
TRahulSam1997 <rahulsamaranayake@yahoo.com>
2021-06-19 17:34:11 +1000

    header preview

--- pages/truthfully/index.js
+++ pages/truthfully/index.js
@@ -4,6 +4,7 @@ import LIVING_TRUTHFULLY from '../../lib/queries/pages/livingTruthfully'
 import client from '../../lib/apollo/client'
 import styles from '../../styles/Home.module.css'
 import styled from 'styled-components';
+import Head from 'next/head'
 
 const Styles = styled.div`
 
@@ -28,6 +29,63 @@ export default function Truthfully ({ pageData }) {
   return (
     <Styles>
       <Layout>
+        <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta charSet="utf-8" />
+          <title>What is Living Truthfully?</title>
+          <meta
+            name="description"
+            content="It always baffled me when I hear people say that studying the humanities is useless and a waste of time."
+          />
+          <meta
+            name="image"
+            content="https://res.cloudinary.com/dkd4xa60a/image/upload/v1623412100/REALM/David_-_The_Death_of_Socrates_iqzfie.jpg"
+            key="ogtitle"
+          />
+          {pageData.content ? (
+            <meta property="og:type" content="article" key="ogtype" />
+          ) : (
+            <meta property="og:type" content="website" key="ogtype" />
+          )}
+          <meta
+            property="og:title"
+            content="What is Living Truthfully?"
+            key="ogtitle"
+          />
+          <meta
+            property="og:description"
+            content="It always baffled me when I hear people say that studying the humanities is useless and a waste of time."
+            key="ogdesc"
+          />
+          <meta
+            property="twitter:card"
+            content="summary_large_image"
+            key="twcard"
+          />
+          {/* <meta name="twitter:creator" content={twitter} key="twhandle" /> */}
+          <meta
+            name="twitter:title"
+            content="What is Living Truthfully?"
+            key="twtitle"
+          />
+          <meta
+            name="twitter:description"
+            content="It always baffled me when I hear people say that studying the humanities is useless and a waste of time."
+            key="twdescription"
+          />
+          <meta
+            name="twitter:image"
+            content="https://res.cloudinary.com/dkd4xa60a/image/upload/v1623412100/REALM/David_-_The_Death_of_Socrates_iqzfie.jpg"
+            key="twimage"
+          />
+          <meta property="og:url" content="https://realm-frontend-v2-gwzj7itzo-trahulsam1997.vercel.app/truthfully" key="ogurl" />
+          <meta
+            property="og:image"
+            content="https://res.cloudinary.com/dkd4xa60a/image/upload/v1623412100/REALM/David_-_The_Death_of_Socrates_iqzfie.jpg"
+            key="ogimage"
+          />
+          <meta property="og:site_name" content="REALM" key="ogsitename" />
+        </Head>
         <main className={styles.main}>
           <h1
             className="font-bold text-3xl md:text-5xl tracking-tight mt-8 mb-8 text-black dark:text-white"
