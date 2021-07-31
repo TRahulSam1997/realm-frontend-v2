bf6c622edb107a9c80b107cd81b85eab6a588473
TRahulSam1997 <rahulsamaranayake@yahoo.com>
2021-07-31 14:35:03 +1000

    log rocket implemented

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,11 @@
 import { ApolloProvider } from '@apollo/client'
 import client from '../lib/apollo/client'
 import '../styles/globals.css'
-
+import LogRocket from 'logrocket';
 import Router from 'next/router';
 import NProgress from 'nprogress';
 
+LogRocket.init('lrslof/realm');
 NProgress.configure( { showSpinner: false } );
 Router.events.on( 'routeChangeStart', () => NProgress.start() );
 Router.events.on( 'routeChangeComplete', () => NProgress.done() );
