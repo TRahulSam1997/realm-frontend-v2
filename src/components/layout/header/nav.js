279f9037bce624e48d9d118f48153fb41a30893a
TRahulSam1997 <rahulsamaranayake@yahoo.com>
2021-08-14 15:25:26 +1000

    responsive navbar

--- src/components/layout/header/nav.js
+++ src/components/layout/header/nav.js
@@ -1,24 +1,42 @@
 import Link from 'next/link'
+import { useState } from 'react';
 
 const Nav = () => {
+    const [active, setActive] = useState(false);
+
+    const handleClick = () => {
+        setActive(!active);
+    };
+
     return (
         <nav className="flex items-center bg-gray-900 p-3 flex-wrap">
-        <a href="#" className="p-2 mr-4 inline-flex items-center">
-            <Link href="/">
-					<a>
-						<img src="https://res.cloudinary.com/dkd4xa60a/image/upload/v1618225098/REALM/logo192_mxoxsc.jpg" alt="Site Logo" width="48" height="48" className="mr-4"/>
-					</a>
-			</Link>
-        </a>
-        <button
-            className="text-white inline-flex p-3 hover:bg-black rounded lg:hidden ml-auto hover:text-white outline-none nav-toggler"
-            data-target="#navigation"
-        >
-            <i className="material-icons">menu</i>
-        </button>
+            <a href="#" className="p-2 mr-4 inline-flex items-center">
+                <Link href="/">
+                        <a>
+                            <img src="https://res.cloudinary.com/dkd4xa60a/image/upload/v1618225098/REALM/logo192_mxoxsc.jpg" alt="Site Logo" width="48" height="48" className="mr-4"/>
+                        </a>
+                </Link>
+            </a>
+            <button
+                className="text-white inline-flex p-3 hover:bg-black rounded lg:hidden ml-auto hover:text-white outline-none nav-toggler"
+                onClick={handleClick}
+            >
+                <svg className="w-6 h-6 text-gray-300 hover:bg-black hover:text-white"
+							x-show="!showMenu"
+							fill="none"
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							stroke-width="2"
+							viewBox="0 0 24 24"
+							stroke="currentColor"
+				>
+					<path d="M4 6h16M4 12h16M4 18h16"></path>
+				</svg>
+            </button>
             <div
-                className="hidden top-navbar w-full lg:inline-flex lg:flex-grow lg:w-auto"
-                id="navigation"
+                className={`${
+                    active ? '' : 'hidden'
+                  }   w-full lg:inline-flex lg:flex-grow lg:w-auto`}
             >
                 <div
                     className="lg:inline-flex lg:flex-row lg:ml-auto lg:w-auto w-full lg:items-center items-start  flex flex-col lg:h-auto"
@@ -45,6 +63,7 @@ const Nav = () => {
                     </Link>
                 </div>
             </div>
+
         </nav>
 
     )
