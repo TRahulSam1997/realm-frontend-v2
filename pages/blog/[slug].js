e077c6662911b540922c6b3919aefea31e314e6d
TRahulSam1997 <rahulsamaranayake@yahoo.com>
2021-09-11 16:22:16 +1000

    back button update

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -198,7 +198,7 @@ export default function Post({ postData }) {
                                 className="blogPostBodyText"
                                 dangerouslySetInnerHTML={{ __html: postData.content }}
                             />
-                            <p className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
+                            <p className="bg-white hover:bg-gray-100 text-gray-700 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
                                 <Link href={`/blog`}>
                                     <a>Back</a>
                                 </Link>
