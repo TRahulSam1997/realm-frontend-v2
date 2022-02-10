cb476d473f193284c4b3c4fce317e6f5bec709c5
TRahulSam1997 <rahulsamaranayake@yahoo.com>
2022-02-10 22:35:22 +1100

    podcast updates

---
+++ pages/podcasts/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../../src/components/layout/index'
+
+export default function index() {
+  return (
+    // <Layout>
+      <div>
+          <h1>index</h1>
+      </div>
+    // </Layout>
+  )
+}
--- src/components/layout/header/nav.js
+++ src/components/layout/header/nav.js
@@ -56,6 +56,11 @@ const Nav = () => {
                             <span>Newsletter</span>
                         </a>
                     </Link>
+                    <Link href={'/podcasts/'}>
+                        <a className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-gray-300 items-center justify-center hover:bg-black hover:text-white">
+                            <span>Podcasts</span>
+                        </a>
+                    </Link>
                     <Link href={'/truthfully/'}>
                         <a className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-gray-300 items-center justify-center hover:bg-black hover:text-white">
                             <span>Living Truthfully</span>
