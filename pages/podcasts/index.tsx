1e1f3cad8ab61eaf115e47277e14779087f47cb3
TRahulSam1997 <rahulsamaranayake@yahoo.com>
2022-02-16 21:08:15 +1100

    podcast page updates

--- pages/podcasts/index.tsx
+++ pages/podcasts/index.tsx
@@ -9,37 +9,10 @@ export default function index() {
           <div className="bg-gray-300 rounded-lg shadow-lg p-5 md:p-20 mx-2">
             <div className="text-center">
               <h2 className="text-4xl tracking-tight leading-10 font-extrabold text-white sm:text-5xl sm:leading-none md:text-6xl">
-                Inner<span className="text-gray-900">REALM</span>
+                Inner<span className="text-gray-900">REALM</span> <span className="text-white text-3xl">Podcast</span>
               </h2>
-              <iframe className='flex items-center mt-10' src="https://anchor.fm/rahul-samaranayake/embed" height="102px" width="400px" frameBorder="0" scrolling="no"></iframe>
-              <p className="text-md md:text-xl mt-10">
-                Salons, workshop, exclusive pieces & mentorship.
-              </p>
-            </div>
-            <div className="flex flex-wrap mt-10 justify-center">
-              <div className="m-3">
-                <a href="https://www.facebook.com/REALM-111744604506248" target="_blank" title="REALM On Facebook"
-                  className="md:w-32 bg-white tracking-wide text-gray-800 font-bold rounded border-2 border-blue-600 hover:border-blue-600 hover:bg-blue-600 hover:text-white shadow-md py-2 px-6 inline-flex items-center">
-                  <span className="mx-auto">Facebook</span>
-                </a>
-              </div>
-              <div className="m-3">
-                <a href="https://twitter.com/" target="_blank" title="REALM On Twitter"
-                  className="md:w-32 bg-white tracking-wide text-gray-800 font-bold rounded border-2 border-blue-500 hover:border-blue-500 hover:bg-blue-500 hover:text-white shadow-md py-2 px-6 inline-flex items-center">
-                  <span className="mx-auto">Twitter</span>
-                </a>
-              </div>
-              <div className="m-3">
-                <a href="https://www.instagram.com/realm.project/" target="_blank" title="REALM On Pinterest"
-                  className="md:w-32 bg-white tracking-wide text-gray-800 font-bold rounded border-2 border-red-600 hover:border-red-600 hover:bg-red-600 hover:text-white shadow-md py-2 px-6 inline-flex items-center">
-                  <span className="mx-auto">Instagram</span>
-                </a>
-              </div>
-              <div className="m-3">
-                <a href="https://medium.com/@trahulsam/" target="_blank" title="REALM On Facebook"
-                  className="md:w-32 bg-white tracking-wide text-gray-800 font-bold rounded border-2 border-black hover:border-orange-500 hover:bg-black hover:text-white shadow-md py-2 px-6 inline-flex items-center">
-                  <span className="mx-auto">Medium</span>
-                </a>
+              <div className="flex items-center justify-center m-10">
+                <iframe src="https://anchor.fm/rahul-samaranayake/embed" height="102px" width="600px" frameBorder="0" scrolling="no"></iframe>
               </div>
             </div>
           </div>
