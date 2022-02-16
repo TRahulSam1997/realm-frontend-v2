d9f6db058c2de13f6feac41a2b371659394d7a2d
TRahulSam1997 <rahulsamaranayake@yahoo.com>
2022-02-16 21:32:11 +1100

    podcast page updates

--- pages/podcasts/index.tsx
+++ pages/podcasts/index.tsx
@@ -11,6 +11,32 @@ export default function index() {
               <h2 className="text-4xl tracking-tight leading-10 font-extrabold text-white sm:text-5xl sm:leading-none md:text-6xl">
                 Inner<span className="text-gray-900">REALM</span> <span className="text-white text-3xl">Podcast</span>
               </h2>
+              <div className="flex flex-wrap mt-10 justify-center">
+              <div className="m-3">
+                <a href="https://youtube.com/playlist?list=PLcJ6nqGscKrcGHWMcVpNzDpk6FfzuDKA-" target="_blank" title="Inner REALM On YouTube"
+                  className="md:w-32 bg-white tracking-wide text-gray-800 font-bold rounded border-2 border-red-600 hover:border-red-600 hover:bg-red-600  hover:text-white shadow-md py-2 px-6 inline-flex items-center">
+                  <span className="mx-auto">YouTube</span>
+                </a>
+              </div>
+              <div className="m-3">
+                <a href="https://twitter.com/" target="_blank" title="REALM On Twitter"
+                  className="md:w-32 bg-white tracking-wide text-gray-800 font-bold rounded border-2 border-green-500 hover:border-green-500 hover:bg-green-500 hover:text-white shadow-md py-2 px-6 inline-flex items-center">
+                  <span className="mx-auto">Spotify</span>
+                </a>
+              </div>
+              <div className="m-3">
+                <a href="https://anchor.fm/rahul-samaranayake" target="_blank" title="REALM On Pinterest"
+                  className="md:w-32 bg-white tracking-wide text-gray-800 font-bold rounded border-2 border-purple-600 hover:border-purple-600 hover:bg-purple-600 hover:text-white shadow-md py-2 px-6 inline-flex items-center">
+                  <span className="mx-auto">Anchor</span>
+                </a>
+              </div>
+              <div className="m-3">
+                <a href="https://medium.com/@trahulsam/" target="_blank" title="REALM On Facebook"
+                  className="md:w-32 bg-white tracking-wide text-gray-800 font-bold rounded border-2 border-black hover:border-orange-500 hover:bg-black hover:text-white shadow-md py-2 px-6 inline-flex items-center">
+                  <span className="mx-auto">P. Index</span>
+                </a>
+              </div>
+            </div>
               <div className="flex items-center justify-center m-10">
                 <iframe src="https://anchor.fm/rahul-samaranayake/embed" height="102px" width="600px" frameBorder="0" scrolling="no"></iframe>
               </div>
